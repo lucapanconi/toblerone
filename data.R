09388afa186d8fea2b901eec272c0b11531a140b
Luca Panconi <104501154+lucapanconi@users.noreply.github.com>
2022-06-13 10:14:14 +0100

    Initial data

---
+++ data.R
@@ -0,0 +1,9 @@
+#' @title Example Images of C. gattii R265 (ordered and disordered channel).
+#'
+#' @description A data set containing two grayscale images of C.gattii R265 representing the intensity in the ordered
+#' and disordered channel. Images are given in matrix form.
+#'
+#' @format A list containing two matrices.
+#' 
+#' @source <https://www.github.com/lucapanconi/toblerone>
+"R265"
